2c16ad2dfa83a31362760249751db66a3fcf886a
Veronica Micheal <vmicheal28@gmail.com>
2022-10-04 17:44:29 -0700

    Fixed no description found for function create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 
- /**
+/**
  * create_array - creates an array of chars,
  * and initialize it with a specific char
  * @size: size of the array to create
@@ -26,6 +26,6 @@ char *create_array(unsigned int size, char c)
 		i++;
 	}
 	*(p + i) = '\0';
-	return (p);
 
+	return (p);
 }
